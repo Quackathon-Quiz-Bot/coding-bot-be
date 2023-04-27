e8bf2636ce5b8dfe210cddff4b4f7140ea16c205
Jessy <japarker0421@gmail.com>
2023-04-27 13:39:26 -0600

    added db user functions

---
+++ db/users.js
@@ -0,0 +1,51 @@
+const { client } = require("./client");
+
+async function createUser({
+  username,
+  correct_answers,
+  incorrect_answers,
+  life_time_score,
+}) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+    INSERT INTO users(username, correct_answers, incorrect_answers, life_time_score)
+    VALUES($1, $2, $3, $4)
+    ON CONFLICT(username) DO NOTHING
+    RETURNING *;
+    `,[username, correct_answers, incorrect_answers, life_time_score])
+    return user;
+  } catch (error) {
+    throw error
+  }
+}
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1}`)
+    .join(", ");
+  if (setString.length === 0) {
+    return;
+  }
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `UPDATE users
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+      `,
+      Object.values(fields)
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createUser,
+  updateUser
+}
