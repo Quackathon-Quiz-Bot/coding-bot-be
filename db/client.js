b798a08b928b7a4f0221d89fbb5db04eca8d3664
Jessy <japarker0421@gmail.com>
2023-05-02 13:55:34 -0600

    got client working in both seed and api

--- db/client.js
+++ db/client.js
@@ -1,18 +1,17 @@
 const { Client } = require("pg");
 const postgres = require('postgres');
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID, URL } = process.env;
-// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
 
 const client = new Client({
-
-  connectionString:
-  URL || "postgres://localhost:5432/coding_quiz_bot_be",
+  connectionString: `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&options=project=${ENDPOINT_ID}` || "postgres://localhost:5432/coding_quiz_bot_be?sslmode=require",
   ssl:
     process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
+      ? {
+          rejectUnauthorized: false,
+
+        }
       : undefined,
-
-
 });
 
 
--- index.js
+++ index.js
@@ -3,19 +3,8 @@ require('dotenv').config();
 const {client}= require('./db/client');
 client.connect();
 
-// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
-
-// ;
-
-// async function getPostgresVersion() {
-//   const result = await client`select version()`;
-//   console.log(result);
-// }
-
 const{PORT=8080}=process.env
 
-// getPostgresVersion();
 
 const express =require('express');
 const server = express();
